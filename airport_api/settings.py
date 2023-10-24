fdde3ba14df00cf2d5127625e98f7511ae8858d5
Inna Isareva <inna8110302@gmail.com>
2023-10-24 19:41:59 +0300

    hidden secret key

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -11,9 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-%4m!vq" "2cz#k7!a7lvh6z+ha(+^yx%x4f7n975%gsz^-l5fg*-h"
-)
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
